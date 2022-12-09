48e112154651192196fb05e10ecd035dd4cff4c4
kahosan <15116355657l@gmail.com>
2022-12-09 08:57:34 +0800

    feat: add test

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -1,6 +1,49 @@
 package app
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
 
 func TestSearchText(t *testing.T) {
+	workPath := "./"
+	text := "app"
+
+	var searchText = &SearchText{}
+
+	result, err := searchText.SearchHandler(workPath, text)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var buf bytes.Buffer
+	buf.WriteString(result[0])
+
+	// 验证输出
+	if buf.String() != "文件名 app_test.go" {
+		t.Errorf("unexpected output: %s", buf.String())
+	}
+}
+
+func TestSearchFile(t *testing.T) {
+	workPath := "./"
+	text := "app"
+
+	var searchFile = &SearchFile{}
+
+	result, err := searchFile.SearchHandler(workPath, text)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var buf bytes.Buffer
+	buf.WriteString(result[1])
+
+	// 验证输出
+	if buf.String() != "文件名 app_test.go" {
+		t.Errorf("unexpected output: %s", buf.String())
+	}
 }
